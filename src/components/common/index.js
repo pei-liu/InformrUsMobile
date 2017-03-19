48b569fbd5497bb119b5919f7ab49f3ceb778ebd
Pei Xiong Liu <peixiong.liu@gmail.com>
2017-03-19 15:37:37 -0400

    Copy over common components

---
+++ src/components/common/index.js
@@ -0,0 +1,11 @@
+// naming this file 'index.js' will allow React to automagically look for this file when
+// declaring: `import { widget1, widget2 } from "./components/common"`
+
+// `export * from ...` syntax is used to re-export all exports of the imported module.
+
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
