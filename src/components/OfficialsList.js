45597bd09394f2b9d28217fe8dc15ddecf41d016
Pei Xiong Liu <peixiong.liu@gmail.com>
2017-03-24 01:55:21 -0400

    Create placeholder for OfficialsListItem

--- src/components/OfficialsList.js
+++ src/components/OfficialsList.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
 
+import OfficialsListItem from './OfficialsListItem';
+
 class OfficialsList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.officialsList);
+  }
+
+  renderRow(official) {
+    return <OfficialsListItem official={official} />;
+  }
+
   render() {
     return (
       <View style={{ marginTop: 20 }}>
-        <Text>OfficialsList</Text>
+        <ListView
+          dataSource={this.dataSource}
+          renderRow={this.renderRow}
+        />
       </View>
     );
   }
---
+++ src/components/OfficialsListItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default (official) => {
+  const {
+    id,
+    first_name,
+    last_name
+  } = official;
+  return (
+    <View>
+      <Text>yolo</Text>
+    </View>
+  );
+};
